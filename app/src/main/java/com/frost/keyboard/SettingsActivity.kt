03e11c3cf3bd78aa45be99a9c92f229d5986d9ba
nikhil905060 <nikhilbattu66@gmail.com>
2026-09-06 12:54:52 +0530

    Create SettingsActivity.kt

---
+++ app/src/main/java/com/frost/keyboard/SettingsActivity.kt
@@ -0,0 +1,23 @@
+package com.frost.keyboard
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+        findViewById<Button>(R.id.btnEnable).setOnClickListener {
+            startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS))
+        }
+        findViewById<Button>(R.id.btnSwitch).setOnClickListener {
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showInputMethodPicker()
+        }
+    }
+}
